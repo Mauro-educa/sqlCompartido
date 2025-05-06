849992bce110745a03b24f010eee24e5fe89e07d
Ivan-educacyl <Ivan.siefer.1@educa.jcyl.es>
2025-05-06 13:04:53 +0200

    Update PROYECTO.sql

--- PROYECTO.sql
+++ PROYECTO.sql
@@ -17,3 +17,11 @@ precio double,
 Constraint Codrt foreign key (cod_receta) references receta(codigo),
 Constraint cod_pk primary key (codigo)
 )
+Create table ticket(
+codigo int(3) NOT NULL,
+cod_Lticket int(5) NOT NULL,
+cod_cliente int(5) NOT NULL,
+Constraint CodLt foreign key (cod_Lticket) references linea_ticket(codigo),
+Constraint CodCl foreign key (cod_cliente) references cliente(codigo),
+Constraint cod_pk primary key (codigo)
+)

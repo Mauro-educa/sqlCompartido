239d5ea56346e151f08705b5a8916e2537e409a6
Ivan-educacyl <Ivan.siefer.1@educa.jcyl.es>
2025-05-06 13:01:21 +0200

    Update PROYECTO.sql

--- PROYECTO.sql
+++ PROYECTO.sql
@@ -10,3 +10,10 @@ nombre varchar(50) NOT NULL,
 precio double,
 Constraint cod_pk primary key (codigo)
 )
+Create table Linea_ticket(
+codigo int(5) NOT NULL,
+cod_receta int(3) NOT NULL,
+precio double,
+Constraint Codrt foreign key (cod_receta) references receta(codigo),
+Constraint cod_pk primary key (codigo)
+)

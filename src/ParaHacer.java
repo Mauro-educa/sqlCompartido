19d4f6b82cbc62b947ee51081ec979d46762de24
mnieves.domnav <mnieves.domnav@ED47010950P028.educa.jcyl.es>
2025-05-14 08:59:03 +0200

    Añadida la clase ParaHacer. No hace nada, solo contiene cosas que tenemos pendientes de hacer

---
+++ src/ParaHacer.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ * Lista de cosas que quedan por hacer. Escribe V delante de cada tarea que
+ * haya sido completada para comprobarlo antes de borrarla.
+ * Añade aquello que creas necesario y falte por hacer para no olvidarlo.
+ * Esta clase se borrará antes de entregar.
+ * 
+ * @author mnieves.domnav
+ */
+public class ParaHacer {
+    /**
+     * Lista general de cosas a hacer:
+     * Conectar base de datos al programa
+     * Crear estructura MVC (así podemos modificar los métodos sin tocar la interfaz gráfica)
+     * Crear clase que almacene los métodos (modelo de MVC)
+     * Crear métodos para tomar un pedido al hacer click en él
+     * Crear métodos para añadir un plato a la bandeja en Cocina (preparado para servirlo al cliente)
+     */
+    
+    /**
+     * Clase Pedido:
+     * 
+     */
+    
+    /**
+     * Clase que conecte a la BBDD:
+     * Crear la clase y conectar a la base de datos
+     * calcularPrecio(): recibe un pedido, calcula su precio total a partir de los platos que tiene y hace un set en la clase Pedido.
+     */
+    
+    /**
+     * Interfaz gráfica:
+     * Crear imágenes de los clientes
+     * Crear imágenes de los platos
+     */
+}

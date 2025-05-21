f4ff9c2ba520ba2ecc877d81712e070edf95a683
mnieves.domnav <mnieves.domnav@ED47010950P028.educa.jcyl.es>
2025-05-21 10:28:07 +0200

    Actualizada la lista de cosas para hacer. Por favor, echadle un ojo, hay una tarea urgente de la base de datos.

--- src/Modelo/ParaHacer.java
+++ src/Modelo/ParaHacer.java
@@ -6,37 +6,47 @@ package Modelo;
  */
 
 /**
- * Lista de cosas que quedan por hacer. Escribe ★ delante de cada tarea que
- * haya sido completada.
- * Añade aquello que creas necesario y falte por hacer para no olvidarlo.
+ * LISTA DE COSAS POR HACER. CONTIENE COSAS POR HACER DE TODOS LOS "DEPARTAMENTOS" 
+ * ★ UNA ESTRELLA DELANTE DE UNA TAREA SIGNIFICA QUE YA HA SIDO COMPLETADA
+ * ➤ ESTA FLECHA DELANTE DE UNA TAREA SIGNIFICA QUE ALGUIEN ESTÁ HACIÉNDOLA YA
+ * ⚠ EL SÍMBOLO DE PELIGRO DELANTE DE UNA TAREA SIGNIFICA QUE ES URGENTE Y NECESITA HACERSE CUANTO ANTES
+ * Añadid aquello que creáis necesario y falte por hacer para no olvidarlo.
  * Esta clase se borrará antes de entregar.
  * 
  * @author mnieves.domnav
  */
 public class ParaHacer {
     /**
-     * Lista general de cosas a hacer:
+     * PROGRAMACIÓN:
      * Conectar base de datos al programa
      * ★ Crear estructura MVC (así podemos modificar los métodos sin tocar la interfaz gráfica)
-     * Crear clase que almacene los métodos (modelo de MVC)
-     * Crear métodos para tomar un pedido al hacer click en él
-     * Crear métodos para añadir un plato a la bandeja en Cocina (preparado para servirlo al cliente)
+     * ★ Crear clase que almacene los métodos (modelo de MVC)
+     * tomarPedido(): tomar un pedido al hacer click en él
+     * addPlato(): añadir un plato a la bandeja en Cocina (preparado para servirlo al cliente)
+     * servir(): sirve los platos en la bandeja a un cliente
+     * calcularPrecio(): recibe un pedido, calcula su precio total a partir de los platos que tiene y le suma la propina obtenida según el tiempo empleado, luego lo añade al bote/puntuación.
+     * ★ nuevoCliente(): cambia el cliente por uno nuevo
+     * ➤ nuevoPedido(): genera un nuevo pedido con 1-3 platos aleatorios
+     * ★ Conectar a la base de datos
+     * ★ Hacer consultas para obtener los clientes y almacenarlos en una lista
+     * ➤ Hacer consultas para obtener las recetas y almacenarlas en una lista
+     * ★ Clase Cliente
+     * ★ Clase Receta
+     * ➤ Clase Pedido
      */
     
     /**
-     * Clase Pedido:
-     * 
-     */
-    
-    /**
-     * Clase que conecte a la BBDD:
-     * Crear la clase y conectar a la base de datos
-     * calcularPrecio(): recibe un pedido, calcula su precio total a partir de los platos que tiene y hace un set en la clase Pedido.
-     */
-    
-    /**
-     * Interfaz gráfica:
+     * INTERFAZ GRÁFICA
      * Crear imágenes de los clientes
      * Crear imágenes de los platos
+     * Crear fondos para el mostrador y la cocina
+     * Mejorar el layout general
+     */
+    
+    /**
+     * BASE DE DATOS:
+     * Añadir más clientes
+     * Añadir más pedidos
+     * ⚠ CORRECCIÓN: hacer que la pk de la línea de ticket esté compuesta por el número o id de la línea de ticket + el id del ticket al que pertenece
      */
 }
